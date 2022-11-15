7c715cd7a960cfe3bfb5c531491f128650246c51
trt77 <hubertsoroczynski@gmail.com>
2022-11-15 16:20:05 +0100

    Update GuessNumber.java

--- Zadanie2.3.2/src/GuessNumber.java
+++ Zadanie2.3.2/src/GuessNumber.java
@@ -2,14 +2,13 @@ import java.util.Random;
 
 public class GuessNumber {
 
-    static Random rand = new Random();
-    private static final int N = rand.nextInt(0,1001);
 
     public static void main(String[] args) {
         int N = losuj(0,1001);
         maszynaLosujaca(N,0,1001);
     }
     public static int losuj(int a, int b){
+        Random rand = new Random();
         int c = rand.nextInt(a,b);
         return c;
     }
